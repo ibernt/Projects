82e889fb71a9337d45eebaf73da90b2ebd162051
Ubuntu <ubuntu@ip-172-31-5-32.eu-west-1.compute.internal>
2019-07-04 21:25:30 +0000

    fixed app again

--- twitter/tweetsearchtest/app.py
+++ twitter/tweetsearchtest/app.py
@@ -1,9 +1,12 @@
 # the top of your flask app
+from flask import Flask
 app = Flask(__name__)
+
 # a route example
 @app.route(‘/’)
- def home():
- return “Hello, Jeffrey Box. U R Kool with a K.”
+def home():
+    return “Hello, Jeffrey Box. U R Kool with a K.”
+
 # the bottom of your flask app
 if __name__ == ‘__main__’:
- app.run(debug=True)
+    app.run(debug=True)
--- twitter/tweetsearchtest/webapptest.py
+++
@@ -1,9 +0,0 @@
-# the top of your flask app
-app = Flask(__name__)
-# a route example
-@app.route(‘/’)
- def home():
- return “Hello, Jeffrey Box. U R Kool with a K.”
-# the bottom of your flask app
-if __name__ == ‘__main__’:
- app.run(debug=True)
